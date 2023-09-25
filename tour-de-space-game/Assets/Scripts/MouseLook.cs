a9bbd2319f98e09962fc0d8ec88a21d3f2c9b028
Azfar Shams <azfarshams1998@gmail.com>
2023-09-25 20:35:36 +0600

    Update MouseLook.cs

--- tour-de-space-game/Assets/Scripts/MouseLook.cs
+++ tour-de-space-game/Assets/Scripts/MouseLook.cs
@@ -17,6 +17,11 @@ public class MouseLook : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+
+        playerBody.Rotate(Vector3.up * mouseX);
     }
 }
