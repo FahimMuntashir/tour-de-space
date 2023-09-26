0f2c3792379f4a6fbbe18830f45e245f5fcad3d8
Azfar Shams <azfarshams1998@gmail.com>
2023-09-26 14:39:08 +0600

    Update Interactor.cs

--- tour-de-space-game/Assets/Scripts/Interactor.cs
+++ tour-de-space-game/Assets/Scripts/Interactor.cs
@@ -9,6 +9,9 @@ interface IInteractable
 
 public class Interactor : MonoBehaviour
 {
+    public Transform InteractorSource;
+    public float InteractRange;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +21,17 @@ public class Interactor : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Ray r = new Ray(InteractorSource.position, InteractorSource.forward);
+            if (Physics.Raycast(r, out RaycastHit hitInfo, InteractRange)) 
+            {
+                if (hitInfo.collider.gameObject.TryGetComponent(out IInteractable interactObj))
+                {
+                    interactObj.Interact();
+                }
+            }
+        }
+
     }
 }
