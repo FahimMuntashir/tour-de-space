eb517dde75ff48124bfc2ea4b90210bd1b80d623
Azfar Shams <azfarshams1998@gmail.com>
2023-10-04 23:35:11 +0600

    Update Timer.cs

--- tour-de-space-game/Assets/Scripts/Timer.cs
+++ tour-de-space-game/Assets/Scripts/Timer.cs
@@ -1,12 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
 
 public class Timer : MonoBehaviour
 {
+    public int NextSceneNumber;
+
+    [SerializeField] TextMeshProGUI timerText;
+    [SerializeField] float remainingTime;
+
     // Update is called once per frame
     void Update()
     {
-        
+        if (remainingTime > 0)
+        {
+            remainingTime -= Time.deltaTime;
+        }    
     }
 }
